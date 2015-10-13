77b0f8e5f7a6611f938143584321231a7fe1c67e
FishFuzz99 <fishfuzz99@gmail.com>
2015-10-13 17:44:18 -0600

    express test

--- Hello World/app.js
+++ Hello World/app.js
@@ -2,6 +2,8 @@ var http = require('http');
 http.createServer(function (req, res)
 {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World!\n');
+	res.end('Hello Test!\n');
 }).listen(8080, '127.0.0.1');
 console.log('Server is running at http://127.0.0.1:8080')
+
+// npm install -g nodemon
